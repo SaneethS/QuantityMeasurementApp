f7d14ae91a77540edf08deced0599844c0d9b689
Saneeth S <saneeths@125AMBBLR-Chethan-RV.local>
2021-10-12 20:47:02 +0530

    Activity LifeCycle are added and displayed i logcat

--- QuantityMeasurer/app/src/main/java/com/yml/quantitymeasurer/MainActivity.kt
+++ QuantityMeasurer/app/src/main/java/com/yml/quantitymeasurer/MainActivity.kt
@@ -2,35 +2,42 @@ package com.yml.quantitymeasurer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.i("status","Process Created")
     }
 
     override fun onStart() {
         super.onStart()
+        Log.i("status","Process Started")
         println("Process Started")
     }
 
     override fun onResume() {
         super.onResume()
+        Log.i("status","Process Resumed")
         println("Process Resumed")
     }
 
     override fun onPause() {
         super.onPause()
+        Log.i("status","Process Paused")
         println("Process Paused")
     }
 
     override fun onRestart() {
         super.onRestart()
+        Log.i("status","Process Restarted")
         println("Process Restarted")
     }
 
     override fun onDestroy() {
         super.onDestroy()
+        Log.i("status","Process Destroyed")
         println("Process Destroyed")
     }
 }
